aec3a7ae54cadf69747694eaa5a4fc81b482d979
Jon Stutz <43560665+jstutz4@users.noreply.github.com>
2024-11-24 17:56:16 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,1 +1,9 @@
+# Import the function from selenium_utils.py
+from selenium_utils import get_page_title
 
+def main(event):
+  # Use the function
+  url = "https://maps.churchofjesuschrist.org/"
+  title = get_page_title(url)
+  print(f"The title of the page is: {title}")
+  return "hello world"
