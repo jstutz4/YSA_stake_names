87299c54c1576bd77a1f5c1152e4c1c97f3b14f2
Jon stutz <sportlightingboy@gmail.com>
2024-11-24 21:06:50 -0700

    fix path for chromedriver

--- scrape_stake_name.py
+++ scrape_stake_name.py
@@ -7,7 +7,8 @@ def get_page_title(url):
     chrome_options.add_argument("--headless")  # Run in headless mode
 
     # Specify the path to the pre-installed ChromeDriver
-    service = ChromeService('/path/to/chromedriver')
+    # service = ChromeService('/Users/jonsoccer/documents/chromedriver-mac-arm64/chromedriver')
+    service = ChromeService('/usr/bin/chromedriver')
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get(url)
