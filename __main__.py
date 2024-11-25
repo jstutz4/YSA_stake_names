e0a06fb2d1f8748753c7d56c49252978266f30c9
Jon stutz <sportlightingboy@gmail.com>
2024-11-24 19:32:01 -0700

    add another print

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ from scrape_stake_name import get_page_title
 
 def main(event):
   # Use the function
+  print("we are at the starts")
   url = "https://maps.churchofjesuschrist.org/"
   title = get_page_title(url)
   print(f"The title of the page is: {title}")
